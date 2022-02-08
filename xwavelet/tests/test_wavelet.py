b2f4c0ddd2383b712e3bf79626d7c7e2cf590fd7
Takaya Uchida <takaya@ldeo.columbia.edu>
2022-02-08 21:48:21 +0100

    black

--- xwavelet/tests/test_wavelet.py
+++ xwavelet/tests/test_wavelet.py
@@ -135,14 +135,12 @@ def test_isotropic_ps_slope(chunk, N=512, dL=1.0, amp=1e0, slope=-3.0, xo=5):
         coords={"scale": np.arange(0.5, 10.5, 0.5)},
     )
     Wtheta = xwavelet.dwvlt(theta, s, dim=["y", "x"])
-    iso_ps = (np.abs(Wtheta)**2).mean(["d0", "angle"]) * (
-        xo * Wtheta.scale
-    ) ** -1
+    iso_ps = (np.abs(Wtheta) ** 2).mean(["d0", "angle"]) * (xo * Wtheta.scale) ** -1
     npt.assert_almost_equal(np.ma.masked_invalid(iso_ps).mask.sum(), 0.0)
     y_fit, a, b = xrft.fit_loglog((xo * iso_ps.scale.values[:]) ** -1, iso_ps.values[:])
     npt.assert_allclose(a, slope, atol=0.2)
 
-    iso_ps = xwavelet.wvlt_spectrum(theta, s, dim=["y", "x"], xo=xo).mean(
+    iso_ps = xwavelet.wvlt_power_spectrum(theta, s, dim=["y", "x"], xo=xo).mean(
         ["d0", "angle"]
     )
     npt.assert_almost_equal(np.ma.masked_invalid(iso_ps).mask.sum(), 0.0)
