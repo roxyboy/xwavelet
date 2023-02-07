07fdfa81adf4d7cc93d507ae81d53dd4fcd1f003
Takaya Uchida <tuchida@dhcp212195.coaps.fsu.edu>
2023-02-07 16:21:33 -0500

    inverse

--- xwavelet/tests/test_wavelet.py
+++ xwavelet/tests/test_wavelet.py
@@ -137,13 +137,13 @@ def test_isotropic_ps_slope(chunk, N=128, dL=1.0, amp=1e0, slope=-3.0, xo=50):
     iso_ps = (np.abs(Wtheta) ** 2).mean(["d0", "angle"]) * (Wtheta.scale) ** -1
     npt.assert_almost_equal(np.ma.masked_invalid(iso_ps).mask.sum(), 0.0)
     y_fit, a, b = xrft.fit_loglog(
-        (iso_ps.scale.values[2:-1]) ** -1, iso_ps.values[2:-1]
+        (iso_ps.scale.values[1:-2]) ** -1, iso_ps.values[1:-2]
     )
     npt.assert_allclose(a, slope, atol=0.3)
 
     iso_ps = wvlt_power_spectrum(theta, s, dim=["y", "x"], xo=xo).mean(["d0", "angle"])
     npt.assert_almost_equal(np.ma.masked_invalid(iso_ps).mask.sum(), 0.0)
     y_fit, a, b = xrft.fit_loglog(
-        (iso_ps.scale.values[2:-1]) ** -1, iso_ps.values[2:-1]
+        (iso_ps.scale.values[1:-2]) ** -1, iso_ps.values[1:-2]
     )
     npt.assert_allclose(a, slope, atol=0.3)
